15a9ccf6b72b1196f1fb22e57b65c49124768fc9
Nishank Koul <96309518+nishankkoul@users.noreply.github.com>
2024-06-07 08:50:24 +0530

    Update calculator.go

--- Dockerfile-without-multistage/calculator.go
+++ Dockerfile-without-multistage/calculator.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	fmt.Println("Hi Abhishek.Veeramalla, I am a calculator app ....")
+	fmt.Println("Hi Nishank Koul, I am a calculator app ....")
 
 	for {
 		// Read input from the user
